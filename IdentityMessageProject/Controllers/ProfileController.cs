908c659fcd71dd87b572a71f19e375f7c73798cb
edaagunes <edagunes00@hotmail.com>
2024-12-09 00:28:42 +0300

    Added profile edit page

--- IdentityMessageProject/Controllers/ProfileController.cs
+++ IdentityMessageProject/Controllers/ProfileController.cs
@@ -1,12 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityMessageProject.EntityLayer.Concrete;
+using IdentityMessageProject.Models.Profile;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityMessageProject.Controllers
 {
 	public class ProfileController : Controller
 	{
-		public IActionResult Index()
+		private readonly UserManager<AppUser> _userManager;
+
+		public ProfileController(UserManager<AppUser> userManager)
 		{
-			return View();
+			_userManager = userManager;
+		}
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+			UpdateProfileViewModel viewModel = new UpdateProfileViewModel();
+			viewModel.Name = user.Name;
+			viewModel.Email = user.Email;
+			viewModel.Surname = user.Surname;
+			viewModel.Username = user.UserName;
+			viewModel.ImageUrl = user.ImageUrl;
+
+			return View(viewModel);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(UpdateProfileViewModel model)
+		{
+			var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+			user.Name=model.Name;
+			user.Surname=model.Surname;	
+			user.Email=model.Email;
+			user.UserName = model.Username;
+
+			if (model.Image != null)
+			{
+				var resource = Directory.GetCurrentDirectory();
+				var extension = Path.GetExtension(model.Image.FileName);
+				var imageName = Guid.NewGuid() + extension;
+				var saveLocation = Path.Combine(resource, "wwwroot/userImages", imageName);
+				using (var stream = new FileStream(saveLocation, FileMode.Create))
+				{
+					await model.Image.CopyToAsync(stream);
+				}
+				user.ImageUrl = "/userImages/"+imageName;
+			}
+
+			else
+			{
+				user.ImageUrl = user.ImageUrl;
+			}
+
+			user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, model.Password);
+
+			var result = await _userManager.UpdateAsync(user);
+
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index", "Login");
+			}
+			else
+			{
+				return View();
+			}
+			
 		}
 	}
 }
--- IdentityMessageProject/Models/Profile/UpdateProfileViewModel.cs
+++ IdentityMessageProject/Models/Profile/UpdateProfileViewModel.cs
@@ -4,5 +4,10 @@
 	{
 		public string Name { get; set; }
 		public string Surname { get; set; }
+		public string Email { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string ImageUrl { get; set; }
+		public IFormFile Image { get; set; }
 	}
 }
